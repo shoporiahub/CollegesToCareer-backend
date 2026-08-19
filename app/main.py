ad2478fe6a6fcb387c717fb8688c4c7c3ffd11b3
daniyaal <shophoriahub@mail.com>
2026-08-19 23:53:59 +0530

    sub domain added

--- app/main.py
+++ app/main.py
@@ -46,7 +46,8 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "http://localhost:8000"
+        "http://localhost:8000",
+        "https://collegestocareer.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
